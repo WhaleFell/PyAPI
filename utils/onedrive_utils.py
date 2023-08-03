c7d4a7cc57edce2afc722a2f5ee35d6312af567e
WhaleFell <whalefall9420@outlook.com>
2023-08-03 12:39:50 +0800

    🎈 perf: config model move in to model.py / create onedrive

---
+++ utils/onedrive_utils.py
@@ -0,0 +1,3 @@
+# onedrive utils
+# ref: https://learn.microsoft.com/zh-cn/onedrive/developer/?view=odsp-graph-online
+# ref: https://learn.microsoft.com/zh-cn/onedrive/developer/rest-api/getting-started/graph-oauth?view=odsp-graph-online
